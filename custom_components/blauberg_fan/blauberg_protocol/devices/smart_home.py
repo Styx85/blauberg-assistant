92956b979cb2a752d61cce6bb8fa6bd5849b9960
Christoph Berliner <32103696+Styx85@users.noreply.github.com>
2023-07-31 23:09:32 +0200

    Update smart_home.py
    KISS very basic

--- custom_components/blauberg_fan/blauberg_protocol/devices/smart_home.py
+++ custom_components/blauberg_fan/blauberg_protocol/devices/smart_home.py
@@ -21,15 +21,8 @@ smart_home = BlaubergDevice(
     name=["Blauberg VENTO Expert A50-1 W V.2", "Blauberg VENTO Expert A85-1 W V.2", "Blauberg VENTO Expert A100-1 W V.2", "Blauberg VENTO Expert Duo A30-1 W V.2", "Blauberg VENTO Expert A30 W V.2"],
     parameter_map={
         Purpose.POWER: SinglePointAction(0x01),
-        Purpose.FAN_SPEED: ComplexAction(
-            parameters=[0x04,0x05],
-            response_parser=lambda response: response[0x04],
-            request_parser=lambda input: {
-                0x04: 1,
-                0x05: 2,
-
-            }
-        ),
-        # TODO: Add other existing purposes as well
-    }
+        # TODO add other existing purposes as well
+    },
+    optional_entity_map=[],
+    attribute_map={},
 )
