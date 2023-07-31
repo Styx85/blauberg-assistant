aad2ee17a51078d13dfbcc191ffb094a003244e8
Christoph Berliner <32103696+Styx85@users.noreply.github.com>
2023-07-31 21:31:27 +0200

    Update devices.py
    ecoventv2 devices

--- custom_components/blauberg_fan/blauberg_protocol/devices/devices.py
+++ custom_components/blauberg_fan/blauberg_protocol/devices/devices.py
@@ -1,5 +1,10 @@
 from collections.abc import Mapping
 from .blauberg_device import BlaubergDevice
 from .smart_wifi import smart_wifi
+from .ecoventv2 import eco_vent
 
-devices: Mapping[int, BlaubergDevice] = {0x600: smart_wifi}
+devices: Mapping[int, BlaubergDevice] = {
+  0x300: eco_vent,
+  0x400: eco_vent,
+  0x500: eco_vent,
+  0x600: smart_wifi}
